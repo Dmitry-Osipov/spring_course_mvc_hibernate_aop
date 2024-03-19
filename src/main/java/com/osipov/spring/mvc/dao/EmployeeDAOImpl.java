e0869424cb2c033c9f28883310160e5f0db864c3
Dmitry Osipov <dimaosipov00@gmail.com>
2024-03-19 14:55:07 +0300

    Studied how to delete entity on the site

--- src/main/java/com/osipov/spring/mvc/dao/EmployeeDAOImpl.java
+++ src/main/java/com/osipov/spring/mvc/dao/EmployeeDAOImpl.java
@@ -3,6 +3,7 @@ package com.osipov.spring.mvc.dao;
 import com.osipov.spring.mvc.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -31,5 +32,19 @@ public class EmployeeDAOImpl implements EmployeeDAO {
         Session session = sessionFactory.getCurrentSession();
 
         return session.get(Employee.class, id);
+    }
+
+    @Override
+    public void deleteEmployee(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Employee> query = session.createQuery("delete from Employee where id = :employeeId");
+        query.setParameter("employeeId", id);
+        query.executeUpdate();
+        /*
+        Более простой вариант:
+        Session session = sessionFactory.getCurrentSession();
+        Employee employee = session.get(Employee.class, id);
+        session.delete(employee);
+        */
     }
 }
